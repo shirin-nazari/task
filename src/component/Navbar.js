1a8225c14ab5511b4dffe29023b5843faa050ee3
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-03-14 08:44:22 +0330

    add pic

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,6 @@
 import AppBar from "@mui/material/AppBar";
 import React from "react";
+import profileUser from "../assets/Profile-PNG-File.png";
 // icon navbar
 import SettingsIcon from "@mui/icons-material/Settings";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
@@ -8,44 +9,79 @@ import MailOutlineIcon from "@mui/icons-material/MailOutline";
 // element
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { makeStyles } from "@mui/styles";
+import { styled } from "@mui/system";
 import Box from "@mui/material/Box";
+
+import TextField from "@mui/material/TextField";
+import { Search } from "@mui/icons-material";
+import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles({
   boxUser: {
     display: "flex",
     paddingLeft: 15,
   },
-  userProfile: {
-    fontSize: "larger",
-    paddingLeft: 10,
+  iconsLeft: {
+    fontSize: 50,
+  },
+  imgProfile: {
+    width: 50,
+    height: 50,
   },
   appbar: {
     background: "#FFFFFF",
     color: "black",
   },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  inputSearch: {
+    borderRadius: "200%",
+    width: 300,
+  },
 });
 export default function Navbar() {
   const classes = useStyles();
+  const SearchBox = styled(TextField)(() => {
+    return {
+      borderRadius: 50,
+    };
+  });
   return (
     <AppBar className={classes.appbar} color="default">
-      <Toolbar>
-        <Box>
-          <SettingsIcon />
-          <NotificationsNoneIcon />
-          <MailOutlineIcon />
-        </Box>
-        <Box className={classes.boxUser}>
+      <Toolbar className={classes.toolbar}>
+        <Box sx={{ display: "flex" }}>
           <Box>
-            <Typography variant="h6" component="h5">
-              Alireza Sandoghizadeh mashhadi
-            </Typography>
-            <Typography variant="p" component="p">
-              designer
-            </Typography>
+            <SettingsIcon className={classes.iconsLeft} />
+            <NotificationsNoneIcon className={classes.iconsLeft} />
+            <MailOutlineIcon className={classes.iconsLeft} />
           </Box>
-          <Box sx={{ height: "50px" }}>
-            <PersonIcon className={classes.userProfile} />
+          <Box className={classes.boxUser}>
+            <Box sx={{ height: "50px" }}>
+              <img
+                src={profileUser}
+                alt="profile-user"
+                className={classes.imgProfile}
+              />
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h5">
+                Alireza Sandoghizadeh mashhadi
+              </Typography>
+              <Typography variant="p" component="p">
+                designer
+              </Typography>
+            </Box>
           </Box>
+        </Box>
+        <Box>
+          <TextField
+            variant="filled"
+            label="Search"
+            className={classes.inputSearch}
+          >
+            <Search />
+          </TextField>
         </Box>
       </Toolbar>
     </AppBar>
