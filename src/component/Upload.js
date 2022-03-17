0d4238b2f9657f02e6f534267cce6c528ed69077
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-03-17 13:07:23 +0330

    add redux

--- src/component/Upload.js
+++ src/component/Upload.js
@@ -1,10 +1,56 @@
-import { Container } from "@mui/material";
-import React from "react";
-
+import { Box, Button, Container, Paper, Typography } from "@mui/material";
+import React, { useState } from "react";
+import ImageIcon from "@mui/icons-material/Image";
+import { add, post } from "../redux/feacture/data/dataSlice";
+import { useDispatch } from "react-redux";
 export default function Upload() {
+  const [upload, setUpload] = useState(false);
+  const dispatch = useDispatch();
+
   return (
-    <Container sx={{ marginTop: 15 }}>
-      <input type="file"></input>
+    <Container
+      sx={{
+        disply: "flex",
+        justifyContent: "center",
+        backgroundColor: "#E9F3FE",
+        textAlign: "center",
+        padding: 20,
+      }}
+    >
+      <Paper
+        sx={{
+          width: 500,
+          margin: "0px auto",
+          padding: 5,
+          border: "4px dashed #E4F0FF ",
+          color: "#1D88FF",
+        }}
+        component="section"
+      >
+        <form>
+          <figure>
+            <ImageIcon sx={{ fontSize: "50px" }} />
+          </figure>
+          <Typography variant="h5" component="p">
+            Drop your image here (:
+          </Typography>
+          <input
+            type="file"
+            multiple
+            onChange={(e) => {
+              console.log(e.target.value);
+              dispatch(post(e.target.value));
+              dispatch(add(e.target.files[0].name));
+              console.log(e.target.files[0].name);
+            }}
+          />
+          <Box sx={{ display: "none" }}>
+            <Typography>uploading</Typography>
+            <Typography>Done!</Typography>
+            <Typography>Error</Typography>
+          </Box>
+        </form>
+      </Paper>
     </Container>
   );
 }
--- src/redux/feacture/data/dataSlice.js
+++ src/redux/feacture/data/dataSlice.js
@@ -1,15 +1,36 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const getData = createAsyncThunk("data/post", async () => {
-  const data = axios({
-    method: "POST",
-    url: "api.tinify.com",
-    auth: "ZTMjKPwYnnbPxzRLxVh7fDqjgKxdZLGb",
-  })
+const getData = createAsyncThunk("data/post", async (data) => {
+  const userName = "shirinnazari";
+  const password = "ZTMjKPwYnnbPxzRLxVh7fDqjgKxdZLGb";
+  const token = Buffer.from(`${userName}:${password}`, "utf8").toString(
+    "base64"
+  );
+  const url = "api.tinify.com/shrink";
+  const postData = axios
+    .post(
+      url,
+      {
+        body: {
+          source: {
+            url: data,
+          },
+        },
+      },
+      {
+        Headers: {
+          Authorization: `Basic ${token}`,
+        },
+      }
+    )
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
-  console.log(data);
+  const getData = axios
+    .get(`${postData}/output/${data}`)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  return getData;
 });
 
 const sliceData = createSlice({
@@ -19,10 +40,18 @@ const sliceData = createSlice({
     status: null,
   },
   reducers: {
+    post(state, { payload }) {
+      getData(payload);
+    },
     [getData.pending]: (state, { payload }) => {
       state.status = "loading";
     },
+    add(state, { payload }) {
+      // for state is array for add to array use Methode push
+      state.image.push(payload);
+    },
     [getData.fulfilled]: (state, { payload }) => {
+      state.image = payload;
       state.status = "success";
     },
     [getData.rejected]: (state, { payload }) => {
@@ -30,4 +59,5 @@ const sliceData = createSlice({
     },
   },
 });
+export const { add, post } = sliceData.actions;
 export default sliceData.reducer;
